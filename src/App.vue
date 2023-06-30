8c9b407e584a7d2a531ed20b98f4475e3e352d09
TaysWeb <snelltararobin@gmail.com>
2023-06-30 16:28:57 +0200

    modal in vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<h1>{{title}}</h1>
+<p>Have a look at our Promotion</p>
+<div v-if="showModal">
+   <modal theme="promotion" @click="toggleModal">
+   <template v-slot:links>
+       <a href="#">Register</a>
+       <a href="#">Find our more </a>
+   </template>
+      <h1>T A R A</h1>
+      <p>come and get up to 50% off</p>
+      </modal>
+</div>
+<button @click="toggleModal">Open Modal</button>
 </template>
+<script>
+import modal from './components/Modal-comp'
+export default {
+  name : 'App',
+  components : {
+   modal
+  },
 
+  data() {
+   return{
+    title:'T A R A',
+    header:'Sign up to win a prize',
+    text: 'come and get up to 50% off ',
+    showModal:false
+   }
+  },
+  methods: {
+    toggleModal() {
+    this.showModal = !this.showModal ;
+    }
+  },
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Modal-comp.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{promotion: theme === 'promotion'}">
+           <slot></slot>
+        <div class="actions">
+        <slot name="links"></slot>
+        </div>
+          
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:['theme'] ,
+    
+    methods: {
+        closeModal (){
+         this.$emit('close');
+        }
+    },
+}
+</script>
+<style scoped>
+    .modal {
+     width:400px;
+     height:200px;
+     margin:100px auto;
+     background:white ;
+     border-radius:10px;
+    }
+    .backdrop {
+     top:0;
+     position:fixed;
+     background:rgba(0, 0, 0, 0.5);
+     width:100%;
+     height:100%;
+    }
+    h1 {
+    color:#03cfb4;
+    border:none;
+    padding:0;
+    }
+    .modal.promotion {
+     background:crimson ;
+     color:white;
+    }
+    .modal.promotion h1 {
+     color:white;
+    }
+    .modal .actions {
+     text-align:center;
+     margin:30px 0 10px 0;
+    }
+    .modal .actions a {
+     color:#333;
+     padding:8px;
+     border: 1px solid #eee;
+     border-radius:4px;
+     text-decoration:none;
+     margin:10px;
+    }
+    .modal.promotion {
+     background:crimson ;
+     color:white;
+    }
+    .modal.promotion h1 {
+     color:white;
+    }
+    .modal.promotion .actions {
+     color:white;
+    }
+    .modal.promotion .actions a {
+     color:white;
+    }
+</style>
